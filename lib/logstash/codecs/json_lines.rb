ffe1fc514316b5429473007c658da4dc80688f24
Suyog Rao <suyog.rao@gmail.com>
2014-11-07 07:43:51 -0800

    minor inline code asciidoc formatting

--- lib/logstash/codecs/json_lines.rb
+++ lib/logstash/codecs/json_lines.rb
@@ -6,21 +6,21 @@ require "logstash/json"
 # This codec will decode streamed JSON that is newline delimited.
 # For decoding line-oriented JSON payload in the redis or file inputs,
 # for example, use the json codec instead.
-# Encoding will emit a single JSON string ending in a '\n'
+# Encoding will emit a single JSON string ending in a `\n`
 class LogStash::Codecs::JSONLines < LogStash::Codecs::Base
   config_name "json_lines"
 
   milestone 3
 
-  # The character encoding used in this codec. Examples include "UTF-8" and
-  # "CP1252"
+  # The character encoding used in this codec. Examples include `UTF-8` and
+  # `CP1252`
   #
-  # JSON requires valid UTF-8 strings, but in some cases, software that
+  # JSON requires valid `UTF-8` strings, but in some cases, software that
   # emits JSON does so in another encoding (nxlog, for example). In
   # weird cases like this, you can set the charset setting to the
   # actual encoding of the text and logstash will convert it for you.
   #
-  # For nxlog users, you'll want to set this to "CP1252"
+  # For nxlog users, you'll want to set this to `CP1252`
   config :charset, :validate => ::Encoding.name_list, :default => "UTF-8"
 
   public
